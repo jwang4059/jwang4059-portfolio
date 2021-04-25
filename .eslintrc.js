9dd1cfaeb2d131bf91af45acaf9cd241cf894fc7
jwang0110 <johnw7@uci.edu>
2021-04-24 19:24:39 -0700

    added eslint and prettier files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": [
+        "airbnb",
+        "prettier"
+    ],
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import",
+        "prettier"
+    ],
+    "rules": {
+    }
+};
