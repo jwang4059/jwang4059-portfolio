71ce2f390365d842de73b25363fd8beffb76930c
jwang0110 <johnw7@uci.edu>
2021-04-24 23:03:19 -0700

    added outline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,26 @@
-import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-  </Layout>
-)
+	<Layout>
+		<Seo title="Home" />
+		{/* <Introduction/>
+    <About/>
+    <Projects/>
+    <Contact/>
+    <Footer/> */}
+		<StaticImage
+			src="../images/gatsby-astronaut.png"
+			width={300}
+			quality={95}
+			formats={["auto", "webp", "avif"]}
+			alt="A Gatsby astronaut"
+			style={{ marginBottom: `1.45rem` }}
+		/>
+	</Layout>
+);
 
-export default IndexPage
+export default IndexPage;
