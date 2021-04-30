2601cdc155a4e1b53a2cd7288b8688738f3ed404
jwang0110 <johnw7@uci.edu>
2021-04-29 22:08:12 -0700

    added animated toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,30 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 import { Link } from "gatsby";
-import { motion } from "framer-motion";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { motion, useCycle } from "framer-motion";
+import { MenuToggle } from "./menuToggle";
 
 interface NavLinkProps {
 	classes: string;
+	component: "link" | "a";
+	href: string;
 	children: ReactNode;
 }
 
-const NavLink = ({ classes, children }: NavLinkProps) => (
-	<li className={`p-4 text-blue-600 ${classes}`}>{children}</li>
-);
+const NavLink = ({ classes, component, href, children }: NavLinkProps) => {
+	const content = (
+		<li className={`p-4 text-blue-600 ${classes}`}>{children}</li>
+	);
+	const renderedLink =
+		component === "link" ? (
+			<Link to={href}>{content}</Link>
+		) : (
+			<a href={href} target="_blank" rel="noreferrer">
+				{content}
+			</a>
+		);
+
+	return renderedLink;
+};
 
 interface NavBarProps {
 	variant?: "mobile" | "desktop";
@@ -24,51 +38,48 @@ const NavBar = ({ variant = "desktop" }: NavBarProps) => {
 
 	return (
 		<ul className="text-left">
-			<Link to="/">
-				<NavLink classes={classes}>Home</NavLink>
-			</Link>
-			<Link to="/about">
-				<NavLink classes={classes}>About</NavLink>
-			</Link>
-			<Link to="/contact">
-				<NavLink classes={classes}>Contact</NavLink>
-			</Link>
-			<a
+			<NavLink classes={classes} component="link" href="/">
+				Home
+			</NavLink>
+			<NavLink classes={classes} component="link" href="/about">
+				About
+			</NavLink>
+			<NavLink classes={classes} component="link" href="/contact">
+				Contact
+			</NavLink>
+			<NavLink
+				classes={classes}
+				component="a"
 				href="https://drive.google.com/file/d/1soK-tqpmOFN5UMyeBQ_zVF6Yymgk7uCO/view?usp=sharing"
-				target="_blank"
-				rel="noreferrer"
 			>
-				<NavLink classes={classes}>Resume</NavLink>
-			</a>
+				Resume
+			</NavLink>
 		</ul>
 	);
 };
 
 const Header = () => {
-	const [open, setOpen] = useState(false);
+	const [isOpen, toggleOpen] = useCycle(false, true);
 
 	return (
 		<header>
 			<nav className="fixed min-w-full bg-gray-100 z-10">
 				<div className="flex flex-row items-center p-2 ">
 					<Link to="/" className="flex-grow text-left p-2 text-blue-600">
-						<i className="fa fa-cloud" /> John Wang{" "}
+						<i className="fa fa-cloud" /> John Wang
 					</Link>
-					<div className="p-2">
-						<motion.button
-							className="h-8 w-8 md:hidden"
-							onClick={() => setOpen(!open)}
-							whileHover={{ scale: 1.2 }}
-							transition={{ type: "spring", duration: 1, stiffness: 200 }}
-						>
-							{!open ? <MenuIcon /> : <XIcon />}
-						</motion.button>
-						<div className="hidden md:flex">
-							<NavBar />
-						</div>
+					<div className="hidden md:flex">
+						<NavBar />
 					</div>
+					<motion.div
+						className=" md:hidden"
+						initial={false}
+						animate={isOpen ? "open" : "closed"}
+					>
+						<MenuToggle toggle={() => toggleOpen()} />
+					</motion.div>
 				</div>
-				{open && <NavBar variant="mobile" />}
+				{isOpen && <NavBar variant="mobile" />}
 			</nav>
 		</header>
 	);
